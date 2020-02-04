21b70fa1db2b1d14b16563e2843c7bb68dfe621e
Ivan Gribanov <gribanov@garant.ru>
2020-02-04 21:46:06 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module telegbot
+
+go 1.13
+
+require (
+	github.com/Syfaro/telegram-bot-api v4.6.4+incompatible
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	tgbotapi "github.com/Syfaro/telegram-bot-api"
+	"log"
+	"net/http"
+)
+
+func main() {
+	var lastMessage *tgbotapi.Message
+	bot, err := tgbotapi.NewBotAPIWithClient("954724330:AAH7XJVLIOUveij2XTNr6IJgnAuvviZg49c", &http.Client{})
+	if err != nil {
+		log.Panic(err)
+	}
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s][%d] %s", update.Message.From.UserName, update.Message.Date, update.Message.Text)
+
+		if lastMessage != nil && update.Message.Date - lastMessage.Date < 3 && lastMessage.From.UserName == update.Message.From.UserName {
+			messageText := "@" + update.Message.From.UserName + ", сука, задолбал спамить!"
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, messageText)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+		lastMessage = update.Message
+
+	}
+}
