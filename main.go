0e183d9d3e01248b995ec946ac00c15a6b890456
Greabanov Ivan <greabanov@yandex.ru>
2022-01-25 03:56:48 +0400

    Disabling MongoDB

--- main.go
+++ main.go
@@ -22,12 +22,12 @@ func main() {
 	mongoURI := os.Getenv("MONGODB_URI")
 	log.Printf("Mongo Uri %s", mongoURI)
 
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
-	err = client.Ping(ctx, readpref.Primary())
-	if err != nil {
-		log.Panic("Error while creating a client: %s", err)
-	}
+	// ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	// client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+	// err = client.Ping(ctx, readpref.Primary())
+	// if err != nil {
+	// 	log.Panic("Error while creating a client: %s", err)
+	//}
 
 	stats = make(map[tgbotapi.User]UserStat)
 	bot, err := tgbotapi.NewBotAPI(botToken)
